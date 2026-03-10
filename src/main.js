9c10c2e40dc808b62c9e31fa65171620f5c979d6
wbwdxh <wbw121124@126.com>
2026-03-10 17:33:25 +0800

    aa

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 
 // Element Plus (UI 框架)
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 
 // 异步加载markdown-it模块
@@ -22,7 +23,9 @@ export { loadMarkdownIt };
 const app = createApp(App);
 
 // 安装 Element Plus
-app.use(ElementPlus);
+app.use(ElementPlus, {
+	locale: zhCn,
+});
 
 // 注册所有 Element Plus 图标组件以便可以直接在模板中使用
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
