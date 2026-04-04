6448acf188b9ae1cbf926f4e7b9c38c3b59fb388
wbw121124 <143000542+wbw121124@users.noreply.github.com>
2026-04-04 17:15:41 +0800

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,9 +6,9 @@ const PostList = defineAsyncComponent(() => import('./PostList.vue'));
 <template>
 	<main class="component XiaolaiMono">
 		<h2 class="text-2xl font-bold mb-6 LXGWNeoXiHeiPlus">欢迎来到 wbw121124blog</h2>
-		<!--计数器 ID:wp9umuzk 控制面板访问密钥:gap0vog1-->
+		<!--计数器 ID:wp9umuzk 控制面板访问密钥:gap0vog1
 		<img src="https://w.saobby.com/w/wp9umuzk" height="0" width="0"
-			style="position: fixed; z-index: -2147483648; opacity: 0;" alt="网站访问统计" />
+			style="position: fixed; z-index: -2147483648; opacity: 0;" alt="网站访问统计" />-->
 		<p class="text-gray-600 dark:text-gray-400 mb-4">这是一个基于 Vue 3 和 Vite 构建的博客网站。</p>
 	</main>
 	
