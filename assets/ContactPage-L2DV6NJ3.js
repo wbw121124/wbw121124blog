a6058f47d46514bcc24c8150ff307f967f2c9e59
wbw121124 <wbw121124@users.noreply.github.com>
2026-04-11 10:03:29 +0000

    Deploy: a60aa04f122930597ad3880b60451718febd6a44 wbw121124/wbw121124blog@a60aa04f122930597ad3880b60451718febd6a44

--- assets/ContactPage-L2DV6NJ3.js
+++
@@ -1,2 +0,0 @@
-import{k as s,o as t,j as a,d as e,b as o}from"./style-D8EB4uws.js";const r={class:"component"};const n=s({},[["render",function(s,n){return t(),a("main",r,[...n[0]||(n[0]=[e("h2",{class:"text-2xl font-bold mb-6"},"联系",-1),e("p",{class:"text-gray-600 dark:text-gray-400"},[o("可以通过 GitHub 联系，或通过邮箱"),e("a",{href:"mailto:wbw121124@126.com",class:"href hover:underline"},"wbw121124@126.com"),o("联系。")],-1)])])}]]);export{n as default};
-//# sourceMappingURL=ContactPage-L2DV6NJ3.js.map
