216024e413ddac889595c8364a1f6b76b594512a
wbw121124 <wbw121124@163.com>
2026-05-03 13:58:33 +0800

    f

--- src/components/PostPage.vue
+++ src/components/PostPage.vue
@@ -155,7 +155,7 @@ function addCopyButtons() {
 			btn.innerHTML = `<i class="el-icon" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M768 832a128 128 0 0 1-128 128H192A128 128 0 0 1 64 832V384a128 128 0 0 1 128-128v64a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64z"></path><path fill="currentColor" d="M384 128a64 64 0 0 0-64 64v448a64 64 0 0 0 64 64h448a64 64 0 0 0 64-64V192a64 64 0 0 0-64-64zm0-64h448a128 128 0 0 1 128 128v448a128 128 0 0 1-128 128H384a128 128 0 0 1-128-128V192A128 128 0 0 1 384 64"></path></svg></i>`;
 
 			btn.addEventListener('click', () => {
-				navigator.clipboard.writeText(btn.parentElement.querySelector('code').innerText).then(() => {
+				navigator.clipboard.writeText(btn.parentElement.parentElement.querySelector('code').innerText).then(() => {
 					ElNotification({
 						title: '代码已复制到剪贴板',
 						type: 'success',
